fa4293eb74f52ad7a888c87dbe7c8cc2a9f44ebf
Davide Ori <davide.ori87@gmail.com>
2017-11-02 23:28:40 +0100

    added snow automatic mixing functionalities according to snow density. Works with arrays and other iterable objects allowing vectorization

--- refractive/refractive/mixing.py
+++ refractive/refractive/mixing.py
@@ -4,13 +4,15 @@ Implement a few effective medium approximation formulas...
 
 """
 
-def maxwell_garnett(m, mix):
+import numpy as np
+
+def maxwell_garnett(eps, mix):
     """Maxwell-Garnett EMA for the refractive index.
 
     Parameters
     ----------
-        m: Tuple of complex
-            refractive indices of the media.
+        eps: Tuple of complex
+            dielectric permittivity of the media
         mix: Tuple of float
             the volume fractions of the media, len(mix)==len(m)
             (if sum(mix)!=1, these are taken relative to sum(mix))
@@ -27,19 +29,23 @@ def maxwell_garnett(m, mix):
     iteration, and so on.
     """
 
-    if len(m) == 2:
-        cF = float(mix[1]) / (mix[0]+mix[1]) * \
-            (m[1]**2-m[0]**2) / (m[1]**2+2*m[0]**2)
-        er = m[0]**2 * (1.0+2.0*cF) / (1.0-cF)
-        m = np.sqrt(er)
-    else:
-        m_last = maxwell_garnett(m[-2:], mix[-2:])
-        mix_last = mix[-2] + mix[-1]
-        m = maxwell_garnett(m[:-2] + (m_last,), mix[:-2] + (mix_last,))
-    return m
+    cF = mix[1] / (mix[0]+mix[1]) * (eps[1]-eps[0]) / (eps[1]+2*eps[0])
+    return eps[0]*(1.0+2.0*cF) / (1.0-cF)
+#    m = np.sqrt(er)
+    
+#    if len(m) == 2:
+#        cF = float(mix[1]) / (mix[0]+mix[1]) * \
+#            (m[1]**2-m[0]**2) / (m[1]**2+2*m[0]**2)
+#        er = m[0]**2 * (1.0+2.0*cF) / (1.0-cF)
+#        m = np.sqrt(er)
+#    else:
+#        m_last = maxwell_garnett(m[-2:], mix[-2:])
+#        mix_last = mix[-2] + mix[-1]
+#        m = maxwell_garnett(m[:-2] + (m_last,), mix[:-2] + (mix_last,))
+#    return m
 
 
-def bruggeman(m, mix):
+def bruggeman(eps, mix):
     """Bruggeman EMA for the refractive index.
 
     For instructions, see mg_refractive in this module, except this routine
@@ -47,15 +53,16 @@ def bruggeman(m, mix):
     """
     f1 = mix[0]/sum(mix)
     f2 = mix[1]/sum(mix)
-    e1 = m[0]**2
-    e2 = m[1]**2
+    e1 = eps[0] #m[0]**2
+    e2 = eps[1] #m[1]**2
     a = -2*(f1+f2)
     b = (2*f1*e1 - f1*e2 + 2*f2*e2 - f2*e1)
     c = (f1+f2)*e1*e2
-    e_eff = (-b - np.sqrt(b**2-4*a*c))/(2*a)
-    return np.sqrt(e_eff)
+    #e_eff = (-b - np.sqrt(b**2-4*a*c))/(2*a)
+    return (-b - np.sqrt(b**2-4*a*c))/(2*a)
+    #return np.sqrt(e_eff)
 
-def sihvola(m,mix,ni=0.85):
+def sihvola(eps,mix,ni=0.85):
     """Sihvola EMA for the refractive index.
 
     For instructions, see mg_refractive in this module, except this routine
@@ -66,7 +73,28 @@ def sihvola(m,mix,ni=0.85):
     modified EMA is way too complicated to be written and computed efficiently:
     a numerically converging solution is applied instead.
     """
-    return bruggeman(m,mix)
+    return bruggeman(eps,mix)
+    
+################################################################################
 
-def effective_medium_approximation(refractive_indices,volume_fractions,model='sihvola',ni=0.85):
-    return sihvola(refractive_indices,volume_fractions,ni)
+def n(refractive_indices,volume_fractions,model='bruggeman',ni=0.85):
+    if model == 'bruggeman':
+        return np.sqrt(bruggeman(refractive_indices**2,volume_fractions))
+    elif model == 'sihvola':
+        return np.sqrt(sihvola(refractive_indices**2,volume_fractions,ni=ni))
+    elif model == 'maxwell_garnett':
+        return np.sqrt(maxwell_garnett(refractive_indices**2,volume_fractions))
+    else:
+        print('Unknown model, fallback to Bruggeman')
+        return np.sqrt(bruggeman(refractive_indices**2,volume_fractions))
+        
+def eps(dielectric_permittivity,volume_fractions,model='bruggeman',ni=0.85):
+    if model == 'bruggeman':
+        return bruggeman(dielectric_permittivity,volume_fractions)
+    elif model == 'sihvola':
+        return sihvola(dielectric_permittivity,volume_fractions,ni=ni)
+    elif model == 'maxwell_garnett':
+        return maxwell_garnett(dielectric_permittivity,volume_fractions)
+    else:
+        print('Unknown model, fallback to Bruggeman')
+        return bruggeman(dielectric_permittivity,volume_fractions)
--- refractive/refractive/snow.py
+++ refractive/refractive/snow.py
@@ -5,15 +5,20 @@ or maybe even other stuff ...
 
 """
 
-from . import ice mixing #water
+import numpy as np
+from . import ice 
+from . import mixing
+# from . import water
 
-#Ice density in g/cm^3
+#Ice density in [mg/mm^3] [g/cm^3] [kg/dm^3]
 ice_density = 0.9167
 
 
-def n(density):
-    return mixing.n(density)
+def n(temperatures,frequencies,densities,model_mix='bruggeman',model_ice='Matzler_2006'):
+    return np.sqrt(eps(temperatures,frequencies,densities,model_mix=model_mix,model_ice=model_ice))
 
-def eps(density):
-    return mixing.eps(density)
-
+def eps(temperatures,frequencies,densities,model_mix='bruggeman',model_ice='Matzler_2006'):
+    fraction = densities/ice_density
+    eps_ice = ice.eps(temperatures,frequencies)
+    eps_air = complex(1.0,0.0)+0.0*eps_ice
+    return mixing.eps([eps_ice,eps_air],[fraction,1.0-fraction],model=model_mix)
