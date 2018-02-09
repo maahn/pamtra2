48d1dbfdbdbc7eba940591e594d2078312ed74c3
Davide Ori <dori@uni-koeln.de>
2018-02-09 10:12:55 +0100

    Modified install.sh to allow easier coherent installation under python2/3 and installation path

--- install.sh
+++ install.sh
@@ -1,23 +1,53 @@
 #! /usr/bin/env bash 
 set -e
 
+echo "pyPamtra2 installing script"
+echo "USAGE: ./install.sh [-p|--python PYTHON_COMMAND] [-f|--flag FLAG1 -f|--flag FLAG2 -f|--flag FLAG3 ...]"
+echo "TYPICAL command: ./install.sh -p python3 -f --user"
+echo "This will install the pyPamtra2 subpackages under /$HOME/.local/lib/python3.X/ which is already in your PYTHONPATH on most unix systems"
+echo "This script is not robust enough to accept any parameter. Anything different from -p -f cause the script to freeze"
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+PYTHON='python'
+
+case $key in
+    -p|--python)
+    PYTHON="$2"
+    shift # past argument
+    shift # past value
+    ;;&
+    -f|--flag)
+    FLAGS+=" $2"
+    shift # past argument
+    shift # past value
+    ;;&
+esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+echo "python command " $PYTHON
+echo "passed flags " $FLAGS
 
 cd pyPamtraRadarSimulator
-python setup.py install
+$PYTHON setup.py install $FLAGS
 cd ../
 
 cd pyPamtraRadarMoments
-python setup.py install
+$PYTHON setup.py install $FLAGS
 cd ../
 
 cd pyPamtra2
-python setup.py install
+$PYTHON setup.py install $FLAGS
 cd ../
 
 cd refractive
-python setup.py install
+$PYTHON setup.py install $FLAGS
 cd ../
 
 cd scattering
-python setup.py install
+$PYTHON setup.py install $FLAGS
 cd ../
