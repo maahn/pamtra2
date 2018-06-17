ae7034e0fa6d68cb803795cbe9a18a06263a1080
Maximilian Maahn <maximilian.maahn@colorado.edu>
2018-06-17 16:33:39 -0600

    cleaned up __init__.py

--- pamtra2/libs/__init__.py
+++ pamtra2/libs/__init__.py
@@ -1,10 +1,2 @@
 # -*- coding: utf-8 -*-
-from __future__ import division, absolute_import, print_function
 
-from . import pamgasabs
-# from . import hydrometeors
-# from . import instruments
-# from . import dimensions
-# from . import helpers
-
-# from .core2 import *
